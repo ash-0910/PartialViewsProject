fe9d36791ab96c74cf8c897bfdb714e76ca4b3c6
ash-0910 <A00244471@mycambrian.ca>
2021-12-15 14:39:59 -0500

    added partial views

--- PartialViewsProject/Pages/Index.cshtml.cs
+++ PartialViewsProject/Pages/Index.cshtml.cs
@@ -49,6 +49,16 @@ namespace PartialViewsProject.Pages
                 " and maybe find a little romance with the coder who conceived him."
             });
 
+            films.Add(new Films()
+            {
+                Title = "Avengers: Endgame",
+                Duration = "3h 2m",
+                Description = "Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his" +
+                " oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America " +
+                "and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos " +
+                "-- the evil demigod who decimated the planet and the universe"
+            });
+
         }
 
         public void OnGet()
